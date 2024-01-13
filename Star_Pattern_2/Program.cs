9398524a20ced59ec5b0edc7eb658d52d8937c12
Pramod-Bhalerao <146328095+Pramod-Bhalerao@users.noreply.github.com>
2024-01-13 12:05:13 +0530

    Add files via upload
    13.02.24 Commited

---
+++ Star_Pattern_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Pattern_Design2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows;
+            Console.WriteLine("Enter No Of Rows");
+            rows=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= rows; i++)
+            {
+                for(int j=1; j<=i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                   
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Star_Pattern_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Pattern_Design3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int rows = 1; rows <=10; rows++)
+            {
+                for (int cols = 1; cols<=rows; cols++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+          
+            for (int rows = 10; rows >= 1; rows--)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
