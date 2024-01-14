43995cca2fc3f710df1e42c321dfc445aa1d8032
Pramod-Bhalerao <146328095+Pramod-Bhalerao@users.noreply.github.com>
2024-01-14 11:04:03 +0530

    Add files via upload
    14.01.24 Commited

---
+++ Number_Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for(int j = 1; j <= 5; j++)
+                {
+                    if (i>j || i==j) 
+                    {
+                        Console.Write(i+" "+i);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();     
+        }
+       
+    }
+}
---
+++ Pattern_Design_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Design_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int rows = 10; rows >= 1; rows--)
+            {
+                for (int cols = 1; cols <= rows; cols++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
