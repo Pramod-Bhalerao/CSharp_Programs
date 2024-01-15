fd32857f1ee541b3a5048cc69cf3f7088e458f3c
Pramod-Bhalerao <146328095+Pramod-Bhalerao@users.noreply.github.com>
2024-01-15 13:38:43 +0530

    Add files via upload
    15.01.24 Commited

---
+++ ArrayList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Array_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Methods Of ArrayList :- 
+                    1 Add
+                    2 Remove
+                    3 Clear
+                    4 Insert
+                    5 TrimToSize
+                    6 Sort
+                    7 Reverse*/
+
+            ArrayList array = new ArrayList();
+            array.Add(121);
+            array.Add("Mr Poms");
+            array.Add(255.30);
+            array.Add('A');
+            array.Add("EDUCATION IS IMPORTANT");
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            array.Remove("Mr Poms");
+
+            Console.WriteLine("_________________________");
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            array.Clear();
+            Console.Read();
+        }
+    }
+}
---
+++ Array_Desending/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Desending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=new int[5];
+
+
+            for(int i=0;i<5;i++)
+            {   
+                Console.WriteLine("Enter A number");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for(int i=0; i<4; i++)
+            {
+                for(int j=i+1; j<5; j++)
+                {
+                    if (arr[j] > arr[i])
+                    {
+                        int temp;
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+            }
+            Console.Read();
+
+        }
+    }
+}
