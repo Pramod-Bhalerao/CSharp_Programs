30c91c4599125c576c1f32f3e3ed492608b91f5b
Pramod-Bhalerao <146328095+Pramod-Bhalerao@users.noreply.github.com>
2024-01-18 12:25:35 +0530

    Add files via upload
    18.01.24 Commited

---
+++ Directory info/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Directory_info
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\POMS");
+            FileInfo[] arr = dir.GetFiles();
+
+            foreach(FileInfo fi in arr) 
+            {
+                Console.WriteLine("{0}--------{1}-------{2}",fi.FullName,fi.CreationTime,fi.Directory);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
